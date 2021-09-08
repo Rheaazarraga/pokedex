8e236e061740c179317f8aaedb919868058b6beb
Rheaazarraga <rheaazarraga@gmail.com>
2021-09-08 03:14:09 +0000

    Changing all vars to const

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 //PART I D.O.M. Manipulation
 
 // Select the search button pokeball by its HTML Id Attribute
-var searchBtn = document.getElementById('search-btn'); // search button pokeball
+const searchBtn = document.getElementById('search-btn'); // search button pokeball
 
 // Select the search field input by its HTML Id Attribute
-var inputField = document.getElementById('name-input'); // search field input
+const inputField = document.getElementById('name-input'); // search field input
 
 // Select the pokemon name screen by it's HTML Id Attribute
-var nameScreen = document.getElementById('name-screen'); //pokemon name-screen
+const nameScreen = document.getElementById('name-screen'); //pokemon name-screen
 
 // Select the pokemon image screen by it's HTML Id Attribute
-var imageScreen = document.getElementById('main-screen'); // pokemon image screen
+const imageScreen = document.getElementById('main-screen'); // pokemon image screen
 
 // Select the height and weight screen by it's HTML Id Attribute
-var aboutScreen = document.getElementById('about-screen'); // about-text screen
+const aboutScreen = document.getElementById('about-screen'); // about-text screen
 
 // Select the type screen by it's HTML Id Attribute
-var typeScreen = document.getElementById('type-screen'); // pokemon type screen
+const typeScreen = document.getElementById('type-screen'); // pokemon type screen
 
 // Select the pokemon id number screen by it's HTML Id Attribute
-var idScreen = document.getElementById('id-screen'); // pokemon id number screen
+const idScreen = document.getElementById('id-screen'); // pokemon id number screen
 
 //PART II Fetching the Pokemon Data from the PokeAPI
 
 //Create a function that uses fetch api to call the poke api and search for a specific pokemon and then add the poke api data to the 
 //appropriate HTML elements from PART I
 
-//Let's assign our fucntion to a variable 
-var getPokemonData = (pokemon) => {
+//Let's assign our fucntion to a constiable 
+const getPokemonData = (pokemon) => {
 
   //Call the fetch api and attach a callback function that will turn the response into json
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
@@ -36,7 +36,7 @@ var getPokemonData = (pokemon) => {
     //a callback function to take the data from out newly converted json data and insert it into our appropriate DOM elements
     .then((data) => {
       //create a format appropriate pokemon id in order to plug into the pokemon.com image url
-      var id = ('00' + data.id).slice(-3);
+      const id = ('00' + data.id).slice(-3);
       //change the background image of the image screen element to the appropriate pokemon image
       imageScreen.style.backgroundImage = `url('https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png')`;
       //change the text of the name screen element to the appropriate pokemon name
